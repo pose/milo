1cdbd2e15bad4c5c297351c8ba8d651ede9f6a6a
Javier Centurion <javier.centurion@mulesoft.com>
2013-04-24 14:48:35 -0300

    adding object proxy

--- app/models/service.js
+++ app/models/service.js
@@ -1,11 +1,11 @@
 App.Service = App.Milo.extend({
-	rootElement: 'services',
-	uriTemplate: App.UriTemplate('/services/%@'),
+    rootElement: 'services',
+    uriTemplate: App.UriTemplate('/services/%@'),
 
-	//// TODO: Implement a has-many interface
-	versions: function () {
-		return App.Version.find({
-			serviceId: this.get('id')
-		});
-	}.property().volatile() //// Avoiding Ember Cache
+    //// TODO: Implement a has-many interface
+    versions: function () {
+        return App.Version.find({
+            serviceId: this.get('id')
+        });
+    }.property().volatile() //// Avoiding Ember Cache
 });
